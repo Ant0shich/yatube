7c13ea125383c9c5950f3bf1da46b50774e448d7
Антон Бородин <borodin6746@mail.ru>
2023-02-19 16:03:17 +0300

    new

---
+++ yatube/posts/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from django import forms
+
+from .models import Post, Comment
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ('text', 'group', 'image',)
+        labels = {'text': 'Текст поста', 'group': 'Группа',
+                  'image': 'Изображение'}
+        help_texts = {
+            'text': 'Текст нового поста',
+            'group': 'Группа, к которой будет относиться пост',
+            'image': 'Добавьте изображение'
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        labels = {
+            'text': 'Текст',
+        }
+        help_texts = {
+            'text': 'Текст нового комментария',
+        }
